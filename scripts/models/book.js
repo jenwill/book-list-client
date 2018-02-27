12d8eed9957606813c07649af85c2fd995ba82ef
Josiah Green <JosiahGreen90@gmail.com>
2018-02-27 13:23:52 -0800

    Added Book constructor and methods to book.js

---
+++ scripts/models/book.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var app = {}
+// var __API_URL__ = 'http://localhost:3000';
+var __API_URL__ = 'https://jwp-jg-booklist.herokuapp.com';
+
+(function (module) {
+  // function errorCallback(err) {
+  //   console.error(err);
+  //   module.errorView.initErrorPage(err);
+  // }
+
+  function Book(bookObject) {
+    Object.keys(bookObject).forEach(key => this[key] = bookObject[key]);
+  }
+
+  Book.prototype.toHtml = function () {
+    // let template = Handlebars.compile($('#book-template').text());
+    // return template(this);
+    console.log('to html works');
+  }
+
+  Book.all = [];
+
+  Book.loadAll = rows => {
+    Book.all = rows.sort((a, b) => b.title - a.title).map(book => new Book(book));
+  }
+
+  Book.fetchAll = callback =>
+    $.get(`${__API_URL__}/books`)
+      .then(Book.loadAll)
+      .then(callback)
+      // .catch(errorCallback)
+;
+  module.Book = Book;
+})(app);
