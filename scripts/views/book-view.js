a01c8bddc6c2817b2212254e3f907fd613086787
Josiah Green <JosiahGreen90@gmail.com>
2018-02-28 13:36:40 -0800

    create form functions & fixed duplicates and hooked up nav

--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -5,9 +5,16 @@ var app = app || {};
 (function (module){
   const bookView = {};
 
+  function reset() {
+    $('.container').hide();
+    $('.navigation').slideDown(350);
+  }
+
   bookView.initIndexPage = function () {
     $('.container').hide();
     $('.book-view').show();
+    $('#book-count span').empty()
+    $('#book-list').empty()
     app.Book.all.map(book => $('#book-list').append(book.toHtml()));
     $('#book-count span').append(app.Book.all.length);
   }
@@ -18,9 +25,21 @@ var app = app || {};
     $('#book-detail').append(app.Book.detailToHtml());
   }
 
+  bookView.initAddBook= function () {
+    reset();
+    $('.form-view').show();
+    $('#add-form').on('submit', function (event) {
+      event.preventDefault();
+      let book = {
+        title: event.target.title.value,
+        description: event.target.description.value,
+        author: event.target.author.value,
+        isbn: event.target.isbn.value,
+        image_url: event.target.image_url.value
+      }
+      module.Book.createBook(book);
+    })
+  }
+
   module.bookView = bookView;
 })(app);
-
-// $(function () {
-//   app.Book.fetchAll(app.bookView.initIndexPage);
-// });
