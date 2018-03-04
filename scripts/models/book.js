e115264607083eeb198cf66202428600e8c1166e
Jennifer Williams <jennifer@squareandplum.com>
2018-03-04 10:09:41 -0800

    switch API_URL to Heroku for deployment

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var app = app || {}
-var __API_URL__ = 'http://localhost:3000';
-// var __API_URL__ = 'https://jwp-jg-booklist.herokuapp.com';
+//var __API_URL__ = 'http://localhost:3000';
+var __API_URL__ = 'https://jwp-jg-booklist.herokuapp.com';
 
 (function (module) {
   function errorCallback(err) {
